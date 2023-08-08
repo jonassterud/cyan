661a1a321bc7b12e4dfe09e47a3b15cc1a6f2caf
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-08-08 23:00:02 +0000

    feat: setup tauri mobile with skeleton

---
+++ crates/app/src/lib.rs
@@ -0,0 +1,7 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub async fn run() -> anyhow::Result<()> {
+    tauri::Builder::default()
+        .run(tauri::generate_context!())?;
+
+    Ok(())
+}
---
+++ crates/app/src/main.rs
@@ -0,0 +1,9 @@
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    #[cfg(desktop)]
+    cyan::run().await?;
+
+    Ok(())
+}
